3d26b602ba379bffb1b67293c64d5a13817ad666
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-06-21 04:54:50 +0000

    layout base template for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+class Pup {
+    constructor(totalTime, Cards) {
+    this.cardsArray = cards;
+    this.totalTime = totalTime
+    this.timeRemaining = totalTime
+    }
+};
+
+function ready() {
+
+}
+
+startGame() ;{
+    this.totalClicks = 0;
+    this.timeRemaining = this.totalTime;
+    this.cardToCheck = null;
+    this.cardToCheck = [];
+    this.busy = true;
+}
+
+hideCards() ;{
+
+}
